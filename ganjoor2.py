95a2402fd41b6be28291d96f502ff20dc9726c6c
Bamboozle Scapegoat <89149731+0x27752357@users.noreply.github.com>
2022-05-21 14:33:41 +0430

    Add files via upload

---
+++ ganjoor2.py
@@ -0,0 +1,33 @@
+import requests
+import bs4
+
+def Molana_DivanShams_Ghazal() :
+    Choice = input("Pick a number between [1,2303] : ")
+
+    link = requests.get("https://ganjoor.net/moulavi/shams/ghazalsh/sh" + Choice)
+
+    data_a = bs4.BeautifulSoup(link.content , 'html.parser')
+
+    data_b = data_a.find('div' , attrs={"class" ,"poem"})
+
+    data_c = data_b.find_all('div' , attrs={"class" , "b"})
+
+    for i in data_c :
+        print(i.text)
+    
+
+def Molana_Masnavi_Daftars() :
+    Choice = input("Pick a daftar : ")
+    Choice_2 = input("Pick your number : ")
+    
+    link = requests.get("https://ganjoor.net/moulavi/masnavi/daftar" + Choice + "/sh" + Choice_2)
+
+    data_a = bs4.BeautifulSoup(link.content , 'html.parser')
+
+    data_b = data_a.find('div' , attrs={"class" ,"poem"})
+
+    data_c = data_b.find_all('div' , attrs={"class" , "b"})
+
+    for i in data_c :
+        print(i.text)
+        
