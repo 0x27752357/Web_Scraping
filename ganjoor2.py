4cd87e62acdf37fbe20e00f565b18fb925614916
Bamboozle Scapegoat <89149731+0x27752357@users.noreply.github.com>
2022-05-21 20:27:50 +0430

    Add files via upload

--- ganjoor2.py
+++ ganjoor2.py
@@ -31,3 +31,50 @@ def Molana_Masnavi_Daftars() :
     for i in data_c :
         print(i.text)
         
+
+
+def Saadi_Divan_Ghazaliat() :
+    Choice_2 = input("Pick your number : ")
+    
+    link = requests.get("https://ganjoor.net/saadi/divan/ghazals/sh"+ Choice_2)
+
+    data_a = bs4.BeautifulSoup(link.content , 'html.parser')
+
+    data_b = data_a.find('div' , attrs={"class" ,"poem"})
+
+    data_c = data_b.find_all('div' , attrs={"class" , "b"})
+
+    for i in data_c :
+        print(i.text)
+        
+
+
+def Saadi_Bostan_Niayesh() :
+    Choice = input("Pick a Bob : ")
+    Choice_2 = input("Pick your number : ")
+    
+    link = requests.get("https://ganjoor.net/saadi/boostan/bab" + Choice + "/sh" + Choice_2)
+
+    data_a = bs4.BeautifulSoup(link.content , 'html.parser')
+
+    data_b = data_a.find('div' , attrs={"class" ,"poem"})
+
+    data_c = data_b.find_all('div' , attrs={"class" , "b"})
+
+    for i in data_c :
+        print(i.text)
+        
+def Hafez_Divan_Ghazaliat() :
+    Choice_2 = input("Pick your number [1,495] : ")
+    
+    link = requests.get("https://ganjoor.net/hafez/ghazal/sh"+ Choice_2)
+
+    data_a = bs4.BeautifulSoup(link.content , 'html.parser')
+
+    data_b = data_a.find('div' , attrs={"class" ,"poem"})
+
+    data_c = data_b.find_all('div' , attrs={"class" , "b"})
+
+    for i in data_c :
+        print(i.text)
+        
